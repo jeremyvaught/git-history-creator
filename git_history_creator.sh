5586ec31ab3b21a6fa638dd6b59b7da293c63612
Jeremy Vaught <jeremy@jeremyvaught.com>
2017-06-24 11:41:31 -0600

    ran update, also added TESTMODE to avoid commenting and uncommenting out lines

--- git_history_creator.sh
+++ git_history_creator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #TESTMODE is 1 if true, 0 if false (do it live!)
-TESTMODE=0
+TESTMODE=1
 #COUNTER is how many days back you want to start
 DAYSOFFSET=0
-DAYSTORUN=6
+DAYSTORUN=0
 POSSIBLEUPDATESPERDAY=7
 TIMEZONEOFFSET="-0400"
 
